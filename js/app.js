d5136258e86de6bfc0376be49d0e481312c96ada
Ksennia Kozhukhova <seniurbano@gmail.com>
2018-07-08 14:06:47 +0300

    Make an HTTP post request

--- js/app.js
+++ js/app.js
@@ -1,10 +1,33 @@
 const http = new easyHTTP;
 
 // Get Posts
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, res) {
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, res) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(res);
+//   }
+// });
+
+// http.get('https://jsonplaceholder.typicode.com/posts/1', function(err, res) {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log(res);
+//   }
+// });
+
+// Create Data
+const data = {
+  title: 'Custom Post',
+  body: 'This is a custom post'
+}
+
+// Post Request
+http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, res) {
   if (err) {
-    console.log(err);
-  } else {
-    console.log(res);
-  }
+      console.log(err);
+    } else {
+      console.log(res);
+    }
 })
